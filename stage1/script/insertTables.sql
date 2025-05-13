6d3200e8d89846006607e06df13377d85a9bf91f
efratMIL <144050310+efratMIL@users.noreply.github.com>
2025-05-13 12:41:00 +0300

    Update insertTables.sql

--- stage1/script/insertTables.sql
+++ stage1/script/insertTables.sql
@@ -9,14 +9,14 @@ INSERT INTO Country (CountryId, CountryName, TotalMedals) VALUES (2, 'Canada', 3
 INSERT INTO Country (CountryId, CountryName, TotalMedals) VALUES (3, 'Germany', 40);
 
 -- הכנסת נתונים לטבלת Venue
-INSERT INTO Venue (VenueID, VenueName, Location, Capacity) VALUES (1, 'Stadium A', 'New York', 50000);
-INSERT INTO Venue (VenueID, VenueName, Location, Capacity) VALUES (2, 'Arena B', 'Toronto', 20000);
-INSERT INTO Venue (VenueID, VenueName, Location, Capacity) VALUES (3, 'Pool C', 'Berlin', 10000);
+INSERT INTO Venue (VenueID, VenueName, VenueLocation, Capacity) VALUES (1, 'Stadium A', 'New York', 50000);
+INSERT INTO Venue (VenueID, VenueName, VenueLocation, Capacity) VALUES (2, 'Arena B', 'Toronto', 20000);
+INSERT INTO Venue (VenueID, VenueName, VenueLocation, Capacity) VALUES (3, 'Pool C', 'Berlin', 10000);
 
 -- הכנסת נתונים לטבלת Athlete
-INSERT INTO Athlete (AthleteID, Name, Gender, Birthday, CountryId) VALUES (1, 'John Doe', 'Male', '1995-06-15', 1);
-INSERT INTO Athlete (AthleteID, Name, Gender, Birthday, CountryId) VALUES (2, 'Jane Smith', 'Female', '2000-09-22', 2);
-INSERT INTO Athlete (AthleteID, Name, Gender, Birthday, CountryId) VALUES (3, 'Tom Brown', 'Male', '1998-12-05', 3);
+INSERT INTO Athlete (AthleteID, AthleteName, Gender, Birthday, CountryId) VALUES (1, 'John Doe', 'Male', '1995-06-15', 1);
+INSERT INTO Athlete (AthleteID, AthleteName, Gender, Birthday, CountryId) VALUES (2, 'Jane Smith', 'Female', '2000-09-22', 2);
+INSERT INTO Athlete (AthleteID, AthleteName, Gender, Birthday, CountryId) VALUES (3, 'Tom Brown', 'Male', '1998-12-05', 3);
 
 -- הכנסת נתונים לטבלת Competition
 INSERT INTO Competition (CompetitionId, CompDate, CompetitionName, SportID, VenueID) VALUES (1, '2025-07-01', 'Olympic Basketball', 1, 1);
@@ -24,11 +24,11 @@ INSERT INTO Competition (CompetitionId, CompDate, CompetitionName, SportID, Venu
 INSERT INTO Competition (CompetitionId, CompDate, CompetitionName, SportID, VenueID) VALUES (3, '2025-07-03', 'Grand Slam', 3, 2);
 
 -- הכנסת נתונים לטבלת Ticket
-INSERT INTO Ticket (CardId, CardDate, Price, VenueID) VALUES (1, '2025-07-01', 100, 1);
-INSERT INTO Ticket (CardId, CardDate, Price, VenueID) VALUES (2, '2025-07-02', 80, 3);
-INSERT INTO Ticket (CardId, CardDate, Price, VenueID) VALUES (3, '2025-07-03', 150, 2);
+INSERT INTO Ticket (CardId, CardDate, TicketPrice, VenueID) VALUES (1, '2025-07-01', 100, 1);
+INSERT INTO Ticket (CardId, CardDate, TicketPrice, VenueID) VALUES (2, '2025-07-02', 80, 3);
+INSERT INTO Ticket (CardId, CardDate, TicketPrice, VenueID) VALUES (3, '2025-07-03', 150, 2);
 
--- הכנסת נתונים לטבלת Relationship
-INSERT INTO Relationship (Rank, Medal, CompetitionId, AthleteID) VALUES (1, 'Gold', 1, 1);
-INSERT INTO Relationship (Rank, Medal, CompetitionId, AthleteID) VALUES (2, 'Silver', 2, 2);
-INSERT INTO Relationship (Rank, Medal, CompetitionId, AthleteID) VALUES (3, 'Bronze', 3, 3);
+-- הכנסת נתונים לטבלת AthleteCompetition
+INSERT INTO AthleteCompetition (AthleteRank, Medal, CompetitionId, AthleteID) VALUES (1, 'Gold', 1, 1);
+INSERT INTO AthleteCompetition (AthleteRank, Medal, CompetitionId, AthleteID) VALUES (2, 'Silver', 2, 2);
+INSERT INTO AthleteCompetition (AthleteRank, Medal, CompetitionId, AthleteID) VALUES (3, 'Bronze', 3, 3);
